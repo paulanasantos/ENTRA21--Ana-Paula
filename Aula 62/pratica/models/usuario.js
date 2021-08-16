3253e3c7b174d7bca6fbfa1b77489aca7a7f239d
ANA PAULA DOS SANTOS 1 <ana.santos41@alunos.sc.senac.br>
2021-08-16 17:24:06 -0300

    atualizado

--- Aula 62/pratica/models/usuario.js
+++ Aula 62/pratica/models/usuario.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize")
-const { hashSync } = require("bcrypt")
 const sequelize = require("../database")
  
 const Usuario = sequelize.define("Usuario", {
@@ -19,32 +18,8 @@ const Usuario = sequelize.define("Usuario", {
         type: DataTypes.STRING,
         allowNull: false,
         set(value) {
-            this.setDataValue("senha", hashSync(value, 10))
+            
         }
     }
-});
-// }, {
-//     tableName: "usuarios",
-//     underscored: true
-// });
  
-(async () => {
-    
-    try {
-        // Criando a tabela Usuarios
-        await Usuario.sync({ force: true });
-        console.log('Tabela criada com sucesso!');
- 
-        // Inserindo um usuário
-        const pedro = await Usuario.create({
-            nome: "Pedro",
-            email: "pedro@gmail.com",
-            senha: "123456"
-        })
-        console.log("Usuário criado com sucesso");
-    } catch (err) {
-        console.log('Ocorreu algum erro ao criar a tabela', err);
-    } finally {
-        sequelize.close()
-    }
-})();
+}) 

716d2c4953f48935b72cc802d7824202119dbab4
ANA PAULA DOS SANTOS 1 <ana.santos41@alunos.sc.senac.br>
2021-08-20 15:54:30 -0300

    atualizado

---
+++ teste.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+// Importando os controllers
+const usuariosControllers = require("../controllers/usuariosControllers");
+
+// Importando as validations
+const usuariosValidations = require("../validations/usuariosValidations");
+
+router.get("/", usuariosControllers.getAll);
+
+router.get("/:id", usuariosControllers.getOne);
+
+router.post("/", usuariosValidations.post, usuariosControllers.create);
+
+router.delete("/:id", usuariosControllers.remove);
+
+router.put("/:id", usuariosControllers.update);
+
+module.exports = router;
